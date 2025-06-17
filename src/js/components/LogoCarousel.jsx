248a7a207735f088580359a6d598276d9dbcc8db
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2025-06-17 23:25:53 +0200

    insertado componente carrusel

---
+++ src/js/components/LogoCarousel.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import logo1 from  "../../img/logonormalsinfondo.png";
+import logo2 from  "../../img/logonormalsinfondo.png";
+import logo3 from  "../../img/logonormalsinfondo.png";
+
+const logos = [
+  logo1,
+  logo2,
+  logo3
+];
+
+const LogoCarousel = () => {
+  const settings = {
+    dots: false, 
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0, 
+    cssEase: 'linear', 
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
+  };
+
+  // Duplicate the logos array to create a continuous loop effect
+  const duplicatedLogos = [...logos, ...logos];
+
+  return (
+    <div style={{ width: "80%", margin: "0 auto" }}>
+      <Slider {...settings}>
+        {duplicatedLogos.map((logo, index) => (
+          <div key={index}>
+            <img src={logo} alt={`logo-${index}`} style={{ width: '100%', height: 'auto' }} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default LogoCarousel;
--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "../components/Navbar";
+import LogoCarousel from "../components/LogoCarousel"; // 👈 importa el componente aquí
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+
       <div className="text-center mt-5">
         <h1>Hello Vicent!</h1>
+
         <p>
           <img src={rigoImage} alt="Rigo" />
         </p>
+
         <a href="#" className="btn btn-success">
           If you see this green button... bootstrap is working...
         </a>
+
         <p>
           Made by{" "}
-          <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!
+          <a href="http://www.4geeksacademy.com" target="_blank" rel="noopener noreferrer">
+            4Geeks Academy
+          </a>
+          , with love!
         </p>
+      </div>
+
+      {/* Carrusel de logos debajo del contenido principal */}
+      <div className="my-5">
+        <LogoCarousel />
       </div>
     </div>
   );
 };
 
 export default Home;
+
