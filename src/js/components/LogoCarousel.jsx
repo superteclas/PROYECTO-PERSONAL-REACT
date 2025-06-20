928583822a7b9a4313c0bc1355cf95f0f9e90079
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2025-06-20 09:36:39 +0200

    carrusel con logos

--- src/js/components/LogoCarousel.jsx
+++ src/js/components/LogoCarousel.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import logo1 from  "../../img/logonormalsinfondo.png";
-import logo2 from  "../../img/logonormalsinfondo.png";
-import logo3 from  "../../img/logonormalsinfondo.png";
+import logo1 from  "../../img/logos/capo.png";
+import logo2 from  "../../img/logos/black.jpg";
+import logo3 from  "../../img/logos/consigo.png";
+import logo4 from  "../../img/logos/eventime.webp";
+import logo5 from  "../../img/logos/glitter.jpg";
+import logo6 from  "../../img/logos/Marriot.svg";
+import logo7 from  "../../img/logos/rock.png";
+import logo8 from  "../../img/logos/tribu.png";
+import logo9 from  "../../img/logos/polop.png";
 
 const logos = [
   logo1,
   logo2,
-  logo3
+  logo3,
+  logo4,
+  logo5,
+  logo6, // index 5
+  logo7,
+  logo8, // index 7
+  logo9,
 ];
 
 const LogoCarousel = () => {
@@ -32,13 +44,32 @@ const LogoCarousel = () => {
   const duplicatedLogos = [...logos, ...logos];
 
   return (
-    <div style={{ width: '100vw', overflow: 'hidden' }}>
+    <div style={{ width: '100vw', overflow: 'hidden', backgroundColor: '#f8f9fa', padding: '1rem 0' }}>
       <Slider {...settings}>
-        {duplicatedLogos.map((logo, index) => (
-          <div key={index} style={{ padding: 0 }}>
-            <img src={logo} alt={`logo-${index}`} style={{ width: '100%', maxHeight: '100px', objectFit: 'contain' }} />
-          </div>
-        ))}
+        {duplicatedLogos.map((logo, index) => {
+          // Tamaño base
+          let imgStyle = {
+            width: '100%',
+            maxHeight: '100px',
+            objectFit: 'contain',
+          };
+
+          // Aumentar tamaño de logo 6 (index 5)
+          if (index % logos.length === 5) {
+            imgStyle.maxHeight = '130px';
+          }
+
+          // Disminuir tamaño de logo 8 (index 7)
+          if (index % logos.length === 7) {
+            imgStyle.maxHeight = '70px';
+          }
+
+          return (
+            <div key={index} style={{ padding: 0 }}>
+              <img src={logo} alt={`logo-${index}`} style={imgStyle} />
+            </div>
+          );
+        })}
       </Slider>
     </div>
   );
