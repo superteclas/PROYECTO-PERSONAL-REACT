abf700c7df47062fd137f13d64c9153f84be5aec
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2025-06-23 17:59:14 +0200

    pagina de contactos acabada

--- src/js/pages/Contact.jsx
+++ src/js/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { FaWhatsapp, FaEnvelope, FaInstagram } from "react-icons/fa";
-import contactBg from "../../img/pianoroto.jpg"; // Ajusta esta ruta si es necesario
+import contactBg from "../../img/tecladosblack.jpg"; // Ajusta esta ruta si es necesario
 import "../../styles/contact.css"; // Asegúrate de crear este archivo
 
 const Contact = () => {
@@ -12,7 +12,7 @@ const Contact = () => {
 
       {/* Sección de imagen de fondo con íconos */}
       <div
-        className="contact-hero d-flex flex-column justify-content-center align-items-center text-white text-center"
+        className="contact-hero"
         style={{
           backgroundImage: `url(${contactBg})`,
         }}
@@ -20,21 +20,18 @@ const Contact = () => {
         <h1 className="mb-4">Contacto</h1>
         <div className="contact-icons d-flex gap-4">
           <a
-            href="https://wa.me/+34666164440" // <-- tu número de WhatsApp con código de país
+            href="https://wa.me/+34666164440"
             target="_blank"
             rel="noopener noreferrer"
             className="contact-icon"
           >
             <FaWhatsapp />
           </a>
-          <a
-            href="mailto:alvaroponcemarin@gmail.com" // <-- tu correo
-            className="contact-icon"
-          >
+          <a href="mailto:alvaroponcemarin@gmail.com" className="contact-icon">
             <FaEnvelope />
           </a>
           <a
-            href="https://instagram.com/alvaroponcekeys" // <-- tu Instagram
+            href="https://instagram.com/alvaroponcekeys"
             target="_blank"
             rel="noopener noreferrer"
             className="contact-icon"
