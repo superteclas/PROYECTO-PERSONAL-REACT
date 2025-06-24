079f9220a1a41b288c00b65e9a51ed8da44f3350
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2025-06-24 09:44:20 +0200

    añadida pagina multtimedia

--- src/js/pages/Multimedia.jsx
+++ src/js/pages/Multimedia.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer"; // 👈 importa el Footer
+import { Footer } from "../components/Footer";
+import "../../styles/multimedia.css";
+
 const Multimedia = () => {
   return (
-     <div className="d-flex flex-column min-vh-100">
-              <Navbar />
-    <div className="text-center mt-5">
-      <h1>MULTIMEDIA</h1>
-      <p>Esta es la página MULTIMEDIA.</p>
-    </div>
-    <Footer />
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <div className="text-center mt-5 px-3">
+        <h1>MULTIMEDIA</h1>
+        <p>Explora lo último en vídeos, animaciones y experiencias AR/VR.</p>
+
+        {/* 🎬 Vídeo corto destacado */}
+        <div className="embed-responsive embed-responsive-16by9 my-4">
+          <iframe
+            className="embed-responsive-item"
+            src="https://www.youtube.com/embed/VIDEOID"
+            allowFullScreen
+            title="Vídeo destacado"
+          />
+        </div>
+
+        {/* 🔄 Microinteracción: objeto flotante animado */}
+        <div className="floating-object mt-4">
+          <img src="/images/float-icon.png" alt="Elemento flotante" />
+        </div>
+
+        {/* 🤖 Experiencia interactiva IA/AR (placeholder) */}
+        <div className="immersive-ar my-5 p-4">
+          <p>Pantalla interactiva AR/VR aquí</p>
+        </div>
+
+        {/* 🌿 Illustración orgánica */}
+        <div className="organic-illustration my-4">
+          <img src="/images/illustration.png" alt="Ilustración orgánica" />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
